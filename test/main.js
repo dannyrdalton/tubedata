401db1cd2cec3ff10e9bca6887332c8f75cfb364
Danny Dalton <drdalton@stanford.edu>
2013-09-11 15:08:20 -0700

    added functionality for tubedata.playlistItems.list()

--- test/main.js
+++ test/main.js
@@ -4,8 +4,26 @@ var tubedata = require('../index.js');
 tubedata.setApiKey('AIzaSyCNVhTbydq4uBvnC8LvmRR2cIFegjReevM');
 console.log(tubedata.getApiKey());
 
-tubedata.video.list(['id', 'snippet', 'contentDetails'], {
+/*tubedata.video.list(['id', 'snippet', 'contentDetails'], {
 	id: 'yfRX9u6Vw2o'
 }, function(error, response, body) {
 	console.log(body);
+}); */
+
+/*tubedata.playlist.list(['snippet', 'contentDetails'], { id: 'UUTTSHJ2LsaHQ8FBiNUstEtw'},
+	function(error, response, body) {
+		console.log(body);
+	}
+); */
+
+tubedata.playlistItems.list(['contentDetails'], { 
+	playlistId: 'UUTTSHJ2LsaHQ8FBiNUstEtw'
+}, function(error, reponse, body) {
+	console.log(body);
 });
+
+/*tubedata.channel.list(['contentDetails'], { forUsername: 'LibraryUk'},
+	function(error, response, body) {
+		console.log(body);
+	}
+); */
